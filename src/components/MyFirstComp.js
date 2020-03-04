5ec837883957a2aa3f60b7f0ea18a376e75716e4
Alex91328 <57780963+Alex91328@users.noreply.github.com>
2020-03-04 13:06:38 -0700

    First Commit

---
+++ src/components/MyFirstComp.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class MyFirstComp extends Component {
+    render() {
+        return (<div>HEllo WOrld!
+        </div>);
+    }
+}
+export default MyFirstComp;
